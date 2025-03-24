181cc1c0b899d398da58f97aa0fbbbeff0272cf6
Théo Berr <theo.berr@icloud.com>
2025-03-25 00:13:40 +0100

    :sparkles: created artifact class

---
+++ main.py
@@ -0,0 +1,13 @@
+from artifact import Artifact
+
+artifact = Artifact(
+    artifact_type="Attribute",
+    subtype="Water",
+    main_stat="HP flat",
+    sub_1="Damage Dealt on Fire +%",
+    sub_2="Damage Dealt on Water +%",
+    sub_3="Damage Dealt on Wind +%",
+    sub_4="Damage Dealt on Light +%"
+)
+
+print(artifact)
