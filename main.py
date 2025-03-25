7594f608eff4cf6cbf54874718a3fb1a9761d84b
Théo Berr <theo.berr@icloud.com>
2025-03-25 20:17:40 +0100

    :white_check_mark: manual test in main to display a rune

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from utils import artifacts_completion
+from utils import load_runes_from_json
 
 def main(json_file_path, excel_file_path):
-    artifacts_completion(json_file_path, excel_file_path)
+    runes = load_runes_from_json(json_file_path)
+
+    print(runes[0])
 
 if __name__ == "__main__":
     main(".data/Eascen-12475513.json", ".data/Account Progression Suggestion - Eascen.xlsx")
