30868642dc0f86aa117aac7ae8ac068efe1a7de6
Théo Berr <theo.berr@icloud.com>
2025-03-25 22:08:02 +0100

    :sparkles: completion of giant speed

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from utils import load_runes_from_json
+from utils import load_runes_from_json, giant_speed_completion
 
 def main(json_file_path, excel_file_path):
-    runes = load_runes_from_json(json_file_path)
-
-    print(runes[0])
+    giant_speed_completion(json_file_path, excel_file_path)
 
 if __name__ == "__main__":
     main(".data/Eascen-12475513.json", ".data/Account Progression Suggestion - Eascen.xlsx")
